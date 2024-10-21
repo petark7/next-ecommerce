e30ff93931ca2b64613e0f4d28e7996b2cad2dab
petark7 <petarkk7@gmail.com>
2024-10-21 21:39:54 +0200

    Implement CategoryLIst

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 
@@ -6,7 +7,14 @@ const HomePage = () => {
     <div className="">
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <div className="text-2xl mb-12">Product List</div>
         <ProductList />
+      </div>
+      <div className="mt-24">
+        <h1 className="text-2xl mb-12 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+          Featured Products
+        </h1>
+        <CategoryList />
       </div>
     </div>
   );
---
+++ src/components/CategoryList.tsx
@@ -0,0 +1,185 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const CategoryList = () => {
+  return (
+    <div className="px-8 overflow-x-auto scrollbar-hide">
+      <div className="flex gap-4 md:gap-8">
+        <Link
+          className="w-[230px] flex-shrink-0"
+          href={'/category?name="boots"'}
+        >
+          <div className="flex flex-col gap-4 ">
+            <div className="relative bg-slate-100 w-full h-96 ">
+              <Image
+                className="rounded-xl"
+                fill
+                alt="Product"
+                src={
+                  "https://images.pexels.com/photos/14838905/pexels-photo-14838905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                }
+              />
+            </div>
+
+            <h1 className="tracking-wide text-xl font-light ">Category Name</h1>
+          </div>
+        </Link>
+        <Link
+          className="w-[230px] flex-shrink-0"
+          href={'/category?name="boots"'}
+        >
+          <div className="flex flex-col gap-4 ">
+            <div className="relative bg-slate-100 w-full h-96 ">
+              <Image
+                className="rounded-xl"
+                fill
+                alt="Product"
+                src={
+                  "https://images.pexels.com/photos/14838905/pexels-photo-14838905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                }
+              />
+            </div>
+
+            <h1 className="tracking-wide text-xl font-light ">Category Name</h1>
+          </div>
+        </Link>
+        <Link
+          className="w-[230px] flex-shrink-0"
+          href={'/category?name="boots"'}
+        >
+          <div className="flex flex-col gap-4 ">
+            <div className="relative bg-slate-100 w-full h-96 ">
+              <Image
+                className="rounded-xl"
+                fill
+                alt="Product"
+                src={
+                  "https://images.pexels.com/photos/14838905/pexels-photo-14838905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                }
+              />
+            </div>
+
+            <h1 className="tracking-wide text-xl font-light ">Category Name</h1>
+          </div>
+        </Link>
+        <Link
+          className="w-[230px] flex-shrink-0"
+          href={'/category?name="boots"'}
+        >
+          <div className="flex flex-col gap-4 ">
+            <div className="relative bg-slate-100 w-full h-96 ">
+              <Image
+                className="rounded-xl"
+                fill
+                alt="Product"
+                src={
+                  "https://images.pexels.com/photos/14838905/pexels-photo-14838905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                }
+              />
+            </div>
+
+            <h1 className="tracking-wide text-xl font-light ">Category Name</h1>
+          </div>
+        </Link>
+        <Link
+          className="w-[230px] flex-shrink-0"
+          href={'/category?name="boots"'}
+        >
+          <div className="flex flex-col gap-4 ">
+            <div className="relative bg-slate-100 w-full h-96 ">
+              <Image
+                className="rounded-xl"
+                fill
+                alt="Product"
+                src={
+                  "https://images.pexels.com/photos/14838905/pexels-photo-14838905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                }
+              />
+            </div>
+
+            <h1 className="tracking-wide text-xl font-light ">Category Name</h1>
+          </div>
+        </Link>
+        <Link
+          className="w-[230px] flex-shrink-0"
+          href={'/category?name="boots"'}
+        >
+          <div className="flex flex-col gap-4 ">
+            <div className="relative bg-slate-100 w-full h-96 ">
+              <Image
+                className="rounded-xl"
+                fill
+                alt="Product"
+                src={
+                  "https://images.pexels.com/photos/14838905/pexels-photo-14838905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                }
+              />
+            </div>
+
+            <h1 className="tracking-wide text-xl font-light ">Category Name</h1>
+          </div>
+        </Link>
+        <Link
+          className="w-[230px] flex-shrink-0"
+          href={'/category?name="boots"'}
+        >
+          <div className="flex flex-col gap-4 ">
+            <div className="relative bg-slate-100 w-full h-96 ">
+              <Image
+                className="rounded-xl"
+                fill
+                alt="Product"
+                src={
+                  "https://images.pexels.com/photos/14838905/pexels-photo-14838905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                }
+              />
+            </div>
+
+            <h1 className="tracking-wide text-xl font-light ">Category Name</h1>
+          </div>
+        </Link>
+        <Link
+          className="w-[230px] flex-shrink-0"
+          href={'/category?name="boots"'}
+        >
+          <div className="flex flex-col gap-4 ">
+            <div className="relative bg-slate-100 w-full h-96 ">
+              <Image
+                className="rounded-xl"
+                fill
+                alt="Product"
+                src={
+                  "https://images.pexels.com/photos/14838905/pexels-photo-14838905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                }
+              />
+            </div>
+
+            <h1 className="tracking-wide text-xl font-light ">Category Name</h1>
+          </div>
+        </Link>
+        <Link
+          className="w-[230px] flex-shrink-0"
+          href={'/category?name="boots"'}
+        >
+          <div className="flex flex-col gap-4 ">
+            <div className="relative bg-slate-100 w-full h-96 ">
+              <Image
+                className="rounded-xl"
+                fill
+                alt="Product"
+                src={
+                  "https://images.pexels.com/photos/14838905/pexels-photo-14838905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                }
+              />
+            </div>
+
+            <h1 className="tracking-wide text-xl font-light ">Category Name</h1>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
