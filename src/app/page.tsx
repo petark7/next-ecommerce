40a224ff5e24c46721787e5150825ff5bc09ca4f
5R <petarkk7@gmail.com>
2024-11-04 12:11:10 +0100

    Add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,10 @@ const HomePage = async () => {
         <h1 className="text-2xl mb-12 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
           Categories
         </h1>
-        <CategoryList />
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <CategoryList />
+        </Suspense>
       </div>
     </div>
   );
