8a4592eed85e8d72d3c6bb28d8c3ce3cd8cd3000
petark7 <petarkk7@gmail.com>
2024-11-07 15:03:57 +0100

    Improve UI
    + update search params

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,11 +1,32 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const Filter = () => {
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleFilterChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(name, value);
+    console.log(pathname);
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <div className="flex flex-col gap-6 md:flex-row justify-between items-center">
       <div className="flex gap-3 items-center flex-wrap">
-        <div className="bg-gray-200 gap-3 w-fit p-1 rounded-full">
-          <select className="bg-gray-200" name="type">
+        <div className="flex items-center justify-center bg-gray-200 gap-3 w-fit p-2 rounded-full">
+          <select
+            className="bg-gray-200 text-sm"
+            name="type"
+            onChange={handleFilterChange}
+          >
             <option value="">Type</option>
             <option value="type2">Physical</option>
             <option value="type3">Digital</option>
@@ -13,44 +34,37 @@ const Filter = () => {
         </div>
 
         <input
+          name="minPrice"
           type="text"
           placeholder="min price"
-          className="text-sm ring-1 ring-gray-300 px-2 p-1 w-28 rounded-full"
+          className="text-sm ring-1 ring-inset ring-gray-300 p-2 w-28 rounded-full"
+          onChange={handleFilterChange}
         />
 
         <input
+          name="maxPrice"
           type="text"
           placeholder="max price"
-          className="text-sm ring-1 ring-gray-300 px-2 p-1 w-28 rounded-full"
+          className="text-sm ring-1 ring-inset ring-gray-300 p-2 w-28 rounded-full"
+          onChange={handleFilterChange}
         />
 
-        <div className="bg-gray-200 gap-3 w-fit p-1 rounded-full">
-          <select className="bg-gray-200" name="type">
-            <option value="">Size</option>
-            <option value="type2">Physical</option>
-            <option value="type3">Digital</option>
-          </select>
-        </div>
-
-        <div className="bg-gray-200 gap-3 w-fit p-1 rounded-full">
-          <select className="bg-gray-200" name="type">
-            <option value="">Color</option>
-            <option value="type2">Physical</option>
-            <option value="type3">Digital</option>
-          </select>
-        </div>
-
-        <div className="bg-gray-200 gap-3 w-fit p-1 rounded-full">
-          <select className="bg-gray-200" name="type">
+        <div className="flex items-center justify-center bg-gray-200 gap-3 w-fit p-2  rounded-full">
+          <select
+            className="bg-gray-200 text-sm"
+            name="category"
+            onChange={handleFilterChange}
+          >
             <option value="">Category</option>
-            <option value="type2">Physical</option>
-            <option value="type3">Digital</option>
+            <option value="shoes">Shoes</option>
+            <option value="clothes">Clothes</option>
           </select>
         </div>
 
         <select
-          className="bg-gray-200 gap-3 w-fit p-1 rounded-full"
-          name="type"
+          className="bg-gray-200 gap-3 w-fit p-2 rounded-full text-sm"
+          name="allFilters"
+          onChange={handleFilterChange}
         >
           <option value="">All Filters</option>
           <option value="type2">Physical</option>
@@ -61,12 +75,15 @@ const Filter = () => {
       {/* RIGHT FILTER PART (sort by) */}
       <div className="w-full md:w-fit">
         <select
-          className="ring-1 ring-gray-300 bg-gray-200 bg-transparent p-1 px-2 w-32 rounded-full"
-          name="type"
+          className="text-sm ring-1 ring-gray-300 bg-gray-200 bg-transparent p-2 w-32 rounded-full"
+          name="sortBy"
+          onChange={handleFilterChange}
         >
           <option value="">Sort by</option>
-          <option value="type2">Physical</option>
-          <option value="type3">Digital</option>
+          <option value="asc price">Price (low to high)</option>
+          <option value="desc price">Price (high to low)</option>
+          <option value="asc lastUpdated">Newest</option>
+          <option value="desc lastUpdated">Oldest</option>
         </select>
       </div>
     </div>
